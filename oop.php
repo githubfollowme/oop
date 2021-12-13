7d09a416b48ff63177d9724388e7ef852669be54
githubfollowme <flymayday112@gmail.com>
2021-12-13 11:39:31 +0800

    new file

--- oop.php
+++ oop.php
@@ -1,52 +1,112 @@
 <?php
-class Animal{
+interface Sound{
+    public function sound();
+}
+class Animal implements sound{
+    protected $name='';
+    protected $age=0;
+    protected $heartbeat=0;
 
-    // 這裡只能接固定值 或就像這樣留空
-    public $name="";
-protected $age=0;
-private $heartbeat=0;
-
-public function _construct(){
-    $this->age=rand(10,20);
-    $this->name='john';
-    $this->heartbeat=rand(20,60);
-}
-public function getName(){
-    return $this->name;
-}
-public function getHeartbeat(){
-    return $this->heartbeat;
-}
-public function setName($name){
-    $this->name=$name;
-}
+    public function __construct(){
+        $this->age=rand(10,20);
+        $this->name='john';
+        $this->heartbeat=rand(20,60);
+    }
+    public function sound(){
+        return "gi";
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getHeartbeat(){
+        return $this->heartbeat;
+    }
+    public function setName($name){
+        $this->name=$name;
+    }
 }
 
-// 結果這邊 沒有用方法 比方get set存取 出現紅字
-$animal=new Aniamal;
-// echo $animal->name;
-echo $animal->getName();
-// $animal->name="mack";
+/* $animal=new Animal;
+$dog=new Animal; */
+//echo $animal->name;
+//$animal->name="mack";
+//echo $animal->name;
+//echo $animal->age;
+
+/* echo $animal->getName();
 $animal->setName('mack');
 echo $animal->getName();
 echo $animal->getHeartbeat();
-echo"<hr>";
+echo "<hr>";
 $dog->setName('herry');
-echo $dog->getName();
+echo $dog->getName(); */
 //echo $animal->heartbeat;
 
-class Dog extends Animal{
-protected $hair_color="black";
 
+class Dog extends Animal{
+    protected $hair_color="black";
+
+
+    public function getColor(){
+        return $this->hair_color;
+    }
+    public function sound(){
+        return "汪";
+    }
+    public function setColor($color){
+        return $this->hair_color=$color;
+    }
+
+    public function getName(){
+        return 'my name is '.$this->name;
+    }
+
+}
+class Cat extends Animal{
+    protected $hair_color="black";
+
+
+    public function getColor(){
+        return $this->hair_color;
+    }
+    public function setColor($color){
+        return $this->hair_color=$color;
+    }
+
+    public function getName(){
+        return 'I am '.$this->name;
+    }
+    public function sound(){
+        return "喵";
+    }
 
 }
 
+echo "<h1>狗</h1>";
 $dog=new Dog;
 echo $dog->getName();
 echo "<br>";
 $dog->setName('mack');
 echo "<br>";
 echo $dog->getName();
-echo"<br>";
+echo "<br>";
+echo $dog->getColor();
+echo "<br>";
+echo $dog->sound();
+echo "<br>";
+
+echo "sdfsdfs";
+echo "<h1>貓</h1>";
+$cat=new Cat;
+echo $cat->getName();
+echo "<br>";
+$cat->setName('judy');
+echo "<br>";
+echo $cat->getName();
+echo "<br>";
+echo $cat->getColor();
+echo "<br>";
+echo $cat->sound();
+echo "<br>";
 
 ?>
